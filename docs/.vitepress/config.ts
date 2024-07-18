d463577af17df186ec8055d121f36b87f8f4d07d
轩某Rikka <121035454+XuanRikka@users.noreply.github.com>
2024-07-18 22:31:55 +0800

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -76,7 +76,7 @@ export default defineConfig({
             { text: '大秦', link: '/clubs/DQ' },
             { text: '蘑菇岛', link: '/clubs/MGD' },
             { text: '墨鲸港', link: '/clubs/MJG' },
-            { text: '夏威夷海港镇', link: '/clubs/XWYHGZ' },
+            { text: '星社', link: '/clubs/SC' },
           ]
         }
       ],
