fc64eb2267e3246914915dbefe2cdccb7748d2af
AdvancedK_ <M1150562056@outlook.com>
2024-12-29 00:27:09 +0800

    [+] 社团规范

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -38,6 +38,7 @@ export default defineConfig({
         { text: '☀️服务器介绍', link: '/about' },
         { text: '😆立即加入', link: '/join' },
         { text: '📋玩家公约', link: '/rule' },
+        { text: '🗃️社团规章', link: '/clubrule' },
         { text: '📛附加严打条款', link: '/addition' },
         { text: '🤵玩家筛选', link: '/applicability' },
         { text: '💵赞助支持', link: '/support' },
